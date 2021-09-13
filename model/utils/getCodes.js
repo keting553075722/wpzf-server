2cbd78ba6cc12f0ea56391132213052e5b66a665
zzhe <zzh31215@gmail.com>
2021-09-13 21:51:03 +0800

    getCodes.js getNames.js  update

--- model/utils/getCodes.js
+++ model/utils/getCodes.js
@@ -4,8 +4,6 @@
  * @createTime 2021/4/14
  */
 
-const cityCascade = require("../common-data/city-cascade")
-
 module.exports = function (code) {
     code = code + ""
     if (!(code.length === 6)) {
@@ -15,13 +13,6 @@ module.exports = function (code) {
     let provinceCode = code.substring(0, 2)
     let cityCode = code.substring(2, 4)
     let countyCode = code.substring(4)
-
-    // 遍历找到省
-    // let province = cityCascade.find(x => x.code === provinceCode)
-    // let city
-    // let county
-    // if (province) city = province.children.find(x => x.code === cityCode)
-    // if (city) county = city.children.find(x => x.code === countyCode)
 
     return {
         SDM: provinceCode+"0000",
--- model/utils/getNames.js
+++ model/utils/getNames.js
@@ -4,28 +4,38 @@
  * @createTime 2021/4/7
  */
 
-const cityCascade = require("../common-data/city-cascade")
+const flatCity = require("../common-data/flat-city.json")
 
 module.exports = function (code) {
     code = code + ""
-    if (!(code.length === 6)) {
+    if (!/\d{6}/.test(code)) {
         throw new Error("code格式不正确")
     }
     // 拆解code
-    let provinceCode = code.substring(0, 2)
-    let cityCode = code.substring(2, 4)
-    let countyCode = code.substring(4)
+    let provinceID = code.substring(0, 2)
+    let cityID = code.substring(2, 4)
+    let countyID = code.substring(4)
+    let provinceCode = provinceID + '0000'
+    let cityCode = provinceID + cityID + '00'
+    let countyCode = provinceID + cityID + countyID
 
     // 遍历找到省
-    let province = cityCascade.find(x => x.code === provinceCode)
-    let city
-    let county
-    if (province) city = province.children.find(x => x.code === cityCode)
-    if (city) county = city.children.find(x => x.code === countyCode)
+    let provinceName  = flatCity[provinceCode] ? flatCity[provinceCode].name : ''
+    let cityName  = flatCity[cityCode] ? flatCity[cityCode].name : ''
+    let countyName  = flatCity[countyCode] ? flatCity[countyCode].name : ''
 
-    return {
-        SMC: province["name"],
-        CMC: city["name"],
-        XMC: county["name"]
+    let result = {
+        SMC: provinceName,
+        CMC: cityName,
+        XMC: countyName
     }
+
+    if(cityID == '00' && countyCode == '00') {
+        result.CMC = ''
+        result.XMC = ''
+    } else if(cityID !== '00' && countyCode == '00') {
+        result.XMC = ''
+    }
+
+    return  result
 }
