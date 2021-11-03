36f4fc5dc5e1df6c843e59ed2b6a5a4a0d6c44a9
zzhe <zzh31215@gmail.com>
2021-11-03 14:10:09 +0800

    修改getdistricts

--- routes/assets.js
+++ routes/assets.js
@@ -17,19 +17,32 @@ const {cloneDeep} = require('lodash')
 router.get('/getdistricts', async function (req, res, next) {
   try {
     const {type} = req.query  //type取 1，2，3
+    // let cascadeCity = cloneDeep(cascader)
+    // if (type == '1') {
+    //   delete cascadeCity[0].children
+    // } else if (type == '2') {
+    //   cascadeCity[0].children.forEach(cityItem => {
+    //     delete cityItem.children
+    //   })
+    // } else if (type == '3') {
+    //
+    // } else {
+    //   cascadeCity = []
+    // }
+    const returnRes = {}
     let cascadeCity = cloneDeep(cascader)
-    if (type == '1') {
-      delete cascadeCity[0].children
-    } else if (type == '2') {
-      cascadeCity[0].children.forEach(cityItem => {
-        delete cityItem.children
-      })
-    } else if (type == '3') {
+    delete cascadeCity[0].children
+    returnRes['1'] = cloneDeep(cascadeCity)
 
-    } else {
-      cascadeCity = []
-    }
-    response.responseSuccess(cascadeCity, res)
+    cascadeCity = cloneDeep(cascader)
+    cascadeCity[0].children.forEach(cityItem => {
+      delete cityItem.children
+    })
+    returnRes['2'] = cloneDeep(cascadeCity)
+
+    cascadeCity = cloneDeep(cascader)
+    returnRes['3'] = cloneDeep(cascadeCity)
+    response.responseSuccess(returnRes, res)
   } catch (e) {
     console.log('/assets/getdistricts', e.message)
     response.responseFailed(res)
